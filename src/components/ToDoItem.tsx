3fcd5e9bb4599b0f19c1273864b451df00fdf68d
Harriet Blundell <harriet.blundell27@hotmail.co.uk>
2026-02-04 12:11:27 +0000

    Add ToDoItem tests

--- src/components/ToDoItem.tsx
+++ src/components/ToDoItem.tsx
@@ -16,6 +16,7 @@ const ToDoItem = ({
     <div className="flex py-2 my-1 px-2 rounded-xl justify-between text-[10px] hover:bg-slate-100 dark:bg-slate-800 dark:hover:bg-slate-700  sm:text-sm">
       <div className="flex w-3/4">
         <button
+          role="button"
           className="group w-16 pr-5"
           onClick={() => handleDeleteTask(task.id)}
         >
@@ -24,6 +25,7 @@ const ToDoItem = ({
             height="15"
             src="https://img.icons8.com/material-outlined/24/FA5252/cancel--v1.png"
             alt="delete"
+            aria-label={`Mark ${title} as deleted`}
             className="cursor-pointer block group-hover:hidden"
           />
           <img
@@ -31,21 +33,22 @@ const ToDoItem = ({
             height="15"
             src="https://img.icons8.com/material-rounded/24/FA5252/cancel--v1.png"
             alt="delete hover"
+            aria-label={`Mark ${title} as deleted`}
             className="hidden group-hover:block"
           />
         </button>
-
         <p className="flex w-3/4 text-left">{title}</p>
       </div>
 
       <div className="flex w-1/4">
-        <p className=" ml-auto my-auto"></p>
+        <p className="ml-auto my-auto"></p>
         <span className="inline sm:hidden">
           {task.priority[0].toUpperCase()}
         </span>
         <span className="hidden sm:inline"> {priority}</span>
         <input
           type="checkbox"
+          aria-label={`Mark ${title} as complete`}
           checked={task.completed}
           onChange={() => handleToggleTask(task.id)}
           className="w-8 h-4 ml-auto my-auto sm:mr-2"
--- src/tests/ToDoItem.test.tsx
+++ src/tests/ToDoItem.test.tsx
@@ -1,5 +1,5 @@
 import ToDoItem from "@/components/ToDoItem";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 describe("ToDoItem Component", () => {
   const mockHandleToggleTask = jest.fn();
@@ -12,6 +12,8 @@ describe("ToDoItem Component", () => {
     priority: "Low",
     created_at: new Date().toString(),
   };
+
+  beforeEach(() => jest.clearAllMocks());
 
   it("renders thet ToDoItem component", () => {
     render(
@@ -26,5 +28,48 @@ describe("ToDoItem Component", () => {
     expect(screen.getByText(/Low/i)).toBeInTheDocument();
   });
 
-  it("should toggle a task to true when the completed box is ticked", () => {});
+  it("calls handleToggleTask with the task id when the checkbox is clicked/changed", () => {
+    render(
+      <ToDoItem
+        task={task}
+        handleToggleTask={mockHandleToggleTask}
+        handleDeleteTask={mockHandleDeleteTask}
+      />
+    );
+
+    const checkbox = screen.getByRole("checkbox");
+    const checkboxAriaLabel = screen.getByRole("checkbox", {
+      name: /Mark Make breakfast as complete/i,
+    });
+    fireEvent.click(checkbox);
+
+    expect(mockHandleToggleTask).toHaveBeenCalledTimes(1);
+    expect(mockHandleToggleTask).toHaveBeenCalledWith(task.id);
+    expect(checkboxAriaLabel).toBeInTheDocument();
+  });
+
+  it("calls handleDeleteTask with the task id when the button is clicked", () => {
+    render(
+      <ToDoItem
+        task={task}
+        handleToggleTask={mockHandleToggleTask}
+        handleDeleteTask={mockHandleDeleteTask}
+      />
+    );
+
+    const deleteButton = screen.getByRole("button", {
+      name: /Mark Make breakfast as deleted/i,
+    });
+
+    fireEvent.click(deleteButton);
+
+    expect(mockHandleDeleteTask).toHaveBeenCalledTimes(1);
+    expect(mockHandleDeleteTask).toHaveBeenCalledWith(task.id);
+    expect(deleteButton).toBeInTheDocument();
+  });
 });
+
+/*
+ 	•	child test → assert callback fired
+	•	parent/integration test → assert state + UI update
+*/
