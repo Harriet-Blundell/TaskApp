25c34195280751f611f7ca44df1e354f86c13892
Harriet Blundell <harriet.blundell27@hotmail.co.uk>
2026-02-05 11:53:48 +0000

    WIP: Add a test to check the dropdown options appear after clicking the button

--- src/tests/Dropdown.test.tsx
+++ src/tests/Dropdown.test.tsx
@@ -1,5 +1,5 @@
 import Dropdown from "@/components/Dropdown";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 describe("Dropdown component", () => {
   const mockHandleSelectedPriorityOption = jest.fn();
@@ -17,11 +17,33 @@ describe("Dropdown component", () => {
     expect(screen.queryByText("Medium")).not.toBeInTheDocument();
     expect(screen.queryByText("Low")).not.toBeInTheDocument();
   });
+
+  it("should render the dropdown options after clicking the button", () => {
+    render(
+      <Dropdown
+        handleSelectedPriorityOption={mockHandleSelectedPriorityOption}
+      />
+    );
+
+    const dropdownBtn = screen.getByRole("button", { name: /Open sort menu/i });
+
+    fireEvent.click(dropdownBtn);
+
+    expect(screen.getByText("High")).toBeInTheDocument();
+    expect(screen.getByText("Medium")).toBeInTheDocument();
+    expect(screen.getByText("Low")).toBeInTheDocument();
+  });
 });
 
 /*
 
-- use getByText when you EXPECTED THE ELEMENT to be there
+- use getByText when you expect the element TO BE there
 - If it is missing, that's a test failure
 
+- use queryByText when you expect the element NOT to be there
+- Assert absence without crashing the test
+- queryByText returns null
+
+- If you're asserting presence -> getBy*
+- If you're assertingn absence -> queryBy*
 */
